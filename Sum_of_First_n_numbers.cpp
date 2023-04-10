1830b6f221c37bfd01b3afa8fac1cad766b59db8
Mohd Ayan Khan <76160546+Andryanmak@users.noreply.github.com>
2023-04-10 20:04:13 +0530

    Create Sum_of_First_n_numbers.cpp

---
+++ Sum_of_First_n_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    float sum;
+    cout<<"Enter n : ";
+    cin>>n;
+    sum=n*(n+1)/2;
+    cout<<"Sum of first n numbers : "<<sum;
+    return 0;
+}
